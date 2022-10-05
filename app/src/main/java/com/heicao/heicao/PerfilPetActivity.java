b95fae7f68e99cfd22b124d1ec688412754e6505
Yara Azevedo <72475344+yara-azevedo@users.noreply.github.com>
2022-10-04 22:56:36 -0300

    Telas principais

---
+++ app/src/main/java/com/heicao/heicao/PerfilPetActivity.java
@@ -0,0 +1,13 @@
+package com.heicao.heicao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerfilPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_pet);
+    }
+}
