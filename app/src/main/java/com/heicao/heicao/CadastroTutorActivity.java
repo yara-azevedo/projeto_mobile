d8c75e828c25521c91121e203c8cdfbac238a239
Yara Azevedo <72475344+yara-azevedo@users.noreply.github.com>
2022-10-09 20:48:24 -0300

    Personalização das telas

--- app/src/main/java/com/heicao/heicao/CadastroTutorActivity.java
+++ app/src/main/java/com/heicao/heicao/CadastroTutorActivity.java
@@ -1,13 +1,39 @@
 package com.heicao.heicao;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class CadastroTutorActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_tutor);
+
+        getSupportActionBar().hide();
+
+        button = findViewById(R.id.button2);
+        progressBar = findViewById(R.id.progressBar2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar = findViewById(R.id.progressBar2);
+                progressBar.setVisibility(View.VISIBLE);
+                button = findViewById(R.id.button2);
+                button.setVisibility(View.INVISIBLE);
+                Intent intent = new Intent(CadastroTutorActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/heicao/heicao/SplashActivity.java
+++ app/src/main/java/com/heicao/heicao/SplashActivity.java
@@ -1,13 +1,33 @@
 package com.heicao.heicao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
 
 public class SplashActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, CadastroTutorActivity.class));
+                progressBar = findViewById(R.id.progressBar);
+                progressBar.setVisibility(View.VISIBLE);
+                finish();
+
+            }
+        },5000);
     }
 }
