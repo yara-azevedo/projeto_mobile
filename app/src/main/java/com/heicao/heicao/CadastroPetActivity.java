955257ab25220fbcf3688e1db1cb3888d051e0c4
Yara Azevedo <72475344+yara-azevedo@users.noreply.github.com>
2022-10-10 18:50:59 -0300

    Tela cadastro pet

--- app/src/main/java/com/heicao/heicao/CadastroPetActivity.java
+++ app/src/main/java/com/heicao/heicao/CadastroPetActivity.java
@@ -2,12 +2,30 @@ package com.heicao.heicao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class CadastroPetActivity extends AppCompatActivity {
+
+    private Button button;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_pet);
+
+        getSupportActionBar().hide();
+
+        button = findViewById(R.id.button2);
+        progressBar = findViewById(R.id.progressBar2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
