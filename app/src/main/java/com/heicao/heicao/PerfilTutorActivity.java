96d3dec2e2cdd43517b1b8614ebed5ea0313de2b
Yara Azevedo <72475344+yara-azevedo@users.noreply.github.com>
2022-10-15 01:37:47 -0300

    MainActivity e botões inferiores

--- app/src/main/java/com/heicao/heicao/PerfilTutorActivity.java
+++ app/src/main/java/com/heicao/heicao/PerfilTutorActivity.java
@@ -1,7 +1,9 @@
 package com.heicao.heicao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PerfilTutorActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class PerfilTutorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_tutor);
+    }
+
+    public void abrirPet(View view){
+        Intent intent = new Intent(this, PerfilPetActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void abrirHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void abrirTutor(View view){
+        Intent intent = new Intent(this, PerfilTutorActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/heicao/heicao/SplashActivity.java
+++ app/src/main/java/com/heicao/heicao/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, CadastroPetActivity.class));
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 progressBar = findViewById(R.id.progressBar);
                 progressBar.setVisibility(View.VISIBLE);
                 finish();
